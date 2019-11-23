c8a2111818447bd749220f4452333f1c460f747d
Meredyth Houpos <meredyth.houpos@gmail.com>
2019-11-23 07:35:10 -0500

    merged columns in database, updated expiration date model with new columns

--- server/db/models/expiration-date.js
+++ server/db/models/expiration-date.js
@@ -2,44 +2,14 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const ExpirationDate = db.define("expiration_date", {
-  Name: {
+  name: {
+    type: Sequelize.STRING
+  },
+  keywords: {
     type: Sequelize.TEXT
   },
-  Keywords: {
-    type: Sequelize.TEXT
-  },
-  PantryMin: {
-    type: Sequelize.TEXT
-  },
-  PantryMax: {
-    type: Sequelize.TEXT
-  },
-  PantryMetric: {
-    type: Sequelize.TEXT
-  },
-  RefrigerateMin: {
-    type: Sequelize.INTEGER
-  },
-  RefrigerateMax: {
-    type: Sequelize.INTEGER
-  },
-  RefrigerateMetric: {
-    type: Sequelize.TEXT
-  },
-  RefrigerateTips: {
-    type: Sequelize.TEXT
-  },
-  FreezeMin: {
-    type: Sequelize.INTEGER
-  },
-  FreezeMax: {
-    type: Sequelize.INTEGER
-  },
-  FreezeMetric: {
-    type: Sequelize.TEXT
-  },
-  FreezeTips: {
-    type: Sequelize.TEXT
+  life: {
+    type: Sequelize.STRING
   }
 });
 
