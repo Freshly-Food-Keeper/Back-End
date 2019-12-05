9fbd5b502276dd85a934e95049a51ce95b5cbaab
Emma Munley <emmamunley@gmail.com>
2019-12-05 16:20:58 -0500

    removed comments

--- server/db/models/user-recipe.js
+++ server/db/models/user-recipe.js
@@ -13,25 +13,3 @@ const UserRecipe = db.define('users_recipes', {
 });
 
 module.exports = UserRecipe;
-
-// UserRecipe.beforeCreate(async function(userRecipe) {
-//   try {
-//     const recipe = await Recipe.findByPk(userRecipe.recipeId);
-//     console.log('recipe in getapi', recipe);
-//     const apiId = recipe.apiId;
-//     userRecipe.apiId = apiId;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-
-// UserRecipe.beforeUpdate(async function(userRecipe) {
-//   try {
-//     const recipe = await Recipe.findByPk(userRecipe.recipeId);
-//     console.log('recipe in getapi', recipe);
-//     const apiId = recipe.apiId;
-//     userRecipe.apiId = apiId;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
