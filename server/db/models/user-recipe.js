09f96239b0ef6451cb85138e8f3ea3e3997d8797
Emma Munley <emmamunley@gmail.com>
2019-12-05 16:26:04 -0500

    updated user recipe

--- server/db/models/user-recipe.js
+++ server/db/models/user-recipe.js
@@ -1,15 +1,11 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Recipe = require('./recipe');
 
 const UserRecipe = db.define('users_recipes', {
   timesMade: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
   },
-  // apiId: {
-  //   type: Sequelize.INTEGER,
-  // },
 });
 
 module.exports = UserRecipe;
